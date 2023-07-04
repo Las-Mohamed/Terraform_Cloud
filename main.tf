500e713383be5295abe10a54231e751746504891
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-07-04 12:31:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">=1.3.0"
   required_providers {
     azurerm = {
       "source" = "hashicorp/azurerm"
-      version  = "~>2.0"
+      version  = "3.43.0"
     }
   }
 
+ 
   }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 resource "random_string" "uniquestring" {
   length  = 20
